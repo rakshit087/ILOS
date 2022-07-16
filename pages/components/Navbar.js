08a1362bfdae59b804844c1e894cd54441ef2960
Rakshit <rakshit087@gmail.com>
2022-07-17 01:01:36 +0530

    Added Navbar

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,7 +1,14 @@
+import { FiMoon, FiSun } from "react-icons/fi";
 export const Navbar = ({ darkMode, setDarkMode }) => {
   return (
-    <nav className="flex p-4 justify-end">
-      <button onClick={() => setDarkMode(!darkMode)}>X</button>
+    <nav className="flex justify-end pr-8 h-1/6">
+      <button onClick={() => setDarkMode(!darkMode)}>
+        {!darkMode ? (
+          <FiMoon className="h-7 w-7" />
+        ) : (
+          <FiSun className="text-white h-7 w-7" />
+        )}
+      </button>
     </nav>
   );
 };
