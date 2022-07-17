ec75966c420ff1e47de78d8c6b3436b944f82f6d
Rakshit <rakshit087@gmail.com>
2022-07-17 13:01:43 +0530

    Added GitHUb Login Button

--- pages/components/ActionBox.js
+++ pages/components/ActionBox.js
@@ -1,5 +1,10 @@
-export const ActionBox = () => {
+import { GithubButton } from "./GithubButton";
+import { useState } from "react";
+export const ActionBox = ({ darkMode }) => {
+  const [githubToken, setGithubToken] = useState("");
   return (
-    <div className="my-8 border-2 border-black rounded-lg md:ml-8 h-80 w-80 md:h-96"></div>
+    <div className="flex items-center justify-center my-8 border-2 border-black rounded-lg dark:border-white md:ml-8 h-80 w-80 md:h-96">
+      <GithubButton />
+    </div>
   );
 };
---
+++ pages/components/GithubButton.js
@@ -0,0 +1,7 @@
+export const GithubButton = () => {
+  return (
+    <button className="flex items-center justify-center px-12 py-1 text-white bg-black rounded-md dark:bg-white dark:text-black">
+      <p>Sign In with GitHub</p>
+    </button>
+  );
+};
