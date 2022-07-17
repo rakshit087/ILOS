7098cea421e1797cbfe696143b6349ee715cb3db
Rakshit <rakshit087@gmail.com>
2022-07-18 02:18:28 +0530

    Fixed api error

--- src/pages/api/mint/[token].js
+++ src/pages/api/mint/[token].js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
     res.status(200).json({
       user: user.data.data.viewer.login,
       stargazers: totalStars,
-      ilos: Math.SQRT2(totalStars),
+      ilos: Math.sqrt(totalStars),
     });
   } catch (error) {
     res.status(401).send({
