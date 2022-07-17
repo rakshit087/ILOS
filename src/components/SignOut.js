0aec8e0487f56651515234ea58a0a7fb2d53a703
Rakshit <rakshit087@gmail.com>
2022-07-17 16:52:41 +0530

    Fixed width of buttons

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,11 +1,12 @@
-import { signIn } from "next-auth/react";
-export const GithubButton = () => {
+import { signOut } from "next-auth/react";
+
+export const SignOut = () => {
   return (
     <button
-      onClick={async () => await signIn()}
+      onClick={async () => await signOut()}
       className="flex items-center justify-center w-64 py-1 text-white bg-black rounded-md dark:bg-white dark:text-black"
     >
-      <p>Sign In with GitHub</p>
+      <p>Sign Out</p>
     </button>
   );
 };
