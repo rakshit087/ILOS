2c9c2dddcd409c666744d2c7135ffe325b3f0a57
Rakshit <rakshit087@gmail.com>
2022-07-17 16:33:13 +0530

    Worked on the auth flow

--- src/components/ActionBox.js
+++ src/components/ActionBox.js
@@ -1,10 +1,10 @@
 import { GithubButton } from "./GithubButton";
-import { useState } from "react";
-export const ActionBox = ({ darkMode }) => {
-  const [githubToken, setGithubToken] = useState("");
+import { useSession } from "next-auth/react";
+export const ActionBox = () => {
+  const { data: session } = useSession();
   return (
     <div className="flex items-center justify-center my-8 border-2 border-black rounded-lg dark:border-white md:ml-8 h-80 w-80 md:h-96">
-      <GithubButton />
+      {!session && <GithubButton />}
     </div>
   );
 };
--- src/components/GithubButton.js
+++ src/components/GithubButton.js
@@ -1,6 +1,10 @@
+import { signIn } from "next-auth/react";
 export const GithubButton = () => {
   return (
-    <button className="flex items-center justify-center px-12 py-1 text-white bg-black rounded-md dark:bg-white dark:text-black">
+    <button
+      onClick={async () => await signIn()}
+      className="flex items-center justify-center px-12 py-1 text-white bg-black rounded-md dark:bg-white dark:text-black"
+    >
       <p>Sign In with GitHub</p>
     </button>
   );
